e4dd9ac79145111df79bf298fc0bc84c3ee60447
Richard Betel <emteeoh@gmail.com>
2021-12-07 00:51:59 -0500

    14 stars, 7A is really 7A and B

--- 7/7A.py
+++ 7/7A.py
@@ -12,7 +12,13 @@ def distance(x,g):
 with open("7Ainput.txt") as file:
     positions = [int(x) for line in file for x in line.split(",")]
 
+goal = math.floor(statistics.median(positions))
+print("7A")
+print(goal)
+print(sum(map(lambda x: distance(x, goal), positions)))
+print()
+
 goal = math.floor(statistics.mean(positions))
+print("7B")
 print(goal)
 print(sum(map(lambda x: fuel(distance(x, goal)), positions)))
-print(positions[-1:])
